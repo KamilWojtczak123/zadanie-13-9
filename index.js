dd3605ba2da88526375b14e7959b6735bfeb142a
Kamil Wojtczak <kamil.wojtczak1@gmail.com>
2018-03-12 08:12:44 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var colors = require('colors');
 
-var handlers = require('.modules/handlers'); // nasz moduł
+var handler = require('.modules/handlers'); // nasz moduł
 
 function start() {
     function onRequest(request, response) {
